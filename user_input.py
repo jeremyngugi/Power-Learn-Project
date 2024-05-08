2e72312b8a8d4555b4600f5c1a41c6e6aa1e2217
jeremyngugi <159730071+jeremyngugi@users.noreply.github.com>
2024-05-09 02:22:44 +0300

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -1,6 +1,5 @@
 name = input("What is your name?: ")
 age = input("What is your age?: ")
 location = input("What is your location: ")
-print("Hello World")
-
+print(f"Hello {name}, you are {age} years old and live in {location}.")
       
